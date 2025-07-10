ebc160d070188a4f1b91af82f407cbd896907a7f
Kcald6 <ivankirbymahinay2@gmail.com>
2025-07-10 17:50:21 +0800

    added border

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let countBtn = document.getElementById("countBtn").addEventListener("click",incrementCount)
 let passengerCount = document.getElementById("count-el")
 let count = 0
-
 let resetBtn = document.getElementById("resetBtn").addEventListener("click", resetCount)
 
 function incrementCount(){
